9a9a0f398f1a8f2af889e7ac4395779ac06dae88
epdth <182921547+epdth@users.noreply.github.com>
2026-02-06 18:45:40 +0100

    Enable roll forward, set ContinuousIntegrationBuild for source link

--- build_nuget.sh
+++ build_nuget.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Build the managed project and create a NuGet package
-dotnet build ./Yarax.Managed/Yarax.Managed.csproj -c Release
+dotnet build ./Yarax.Managed/Yarax.Managed.csproj -c Release /p:ContinuousIntegrationBuild=true 
 dotnet pack ./Yarax.Managed/Yarax.Managed.csproj -c Release -o ./nupkgs
 
 # Build the nuget package with all the native dependencies
@@ -12,9 +12,9 @@ chmod +x collect_binaries.sh
 ./collect_binaries.sh
 popd
 
-dotnet build ./Yarax.Native/Yarax.Native.csproj -c Release
+dotnet build ./Yarax.Native/Yarax.Native.csproj -c Release /p:ContinuousIntegrationBuild=true 
 dotnet pack ./Yarax.Native/Yarax.Native.csproj -c Release -o ./nupkgs
 
 # Finally, build the metapackage
-dotnet build ./Yarax/Yarax.csproj -c Release
+dotnet build ./Yarax/Yarax.csproj -c Release /p:ContinuousIntegrationBuild=true 
 dotnet pack ./Yarax/Yarax.csproj -c Release -o ./nupkgs
